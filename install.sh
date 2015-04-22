a9afd5b863e4098fa02414b5479976e82004422c
Li Xin <lixin18@meituan.com>
2015-04-21 22:52:24 -0400

    Add bash_profile & Fix readme

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+work_dir=`dirname $0`
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 rm ~/.vimrc ~/.tmux.conf
-ln -s ~/vimrc/.vimrc ~/.vimrc
+ln -s ~/$work_dir/.vimrc ~/.vimrc
 vim +VundleInstall! +qa
-ln -s ~/vimrc/.tmux.conf ~/.tmux.conf
+ln -s ~/$work_dir/.tmux.conf ~/.tmux.conf
