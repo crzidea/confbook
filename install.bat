bd95bb3c8b2f4cc7668acb7715800031f22d913b
Li Xin <lixin18@meituan.com>
2015-07-16 18:05:53 +0800

    Update readme

--- install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %~dp0
-copy .vimrc %userprofile%\.vimrc 
-git clone https://github.com/gmarik/Vundle.vim.git %userprofile%\.vim\bundle\Vundle.vim
-gvim +VundleInstall! +qa
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-work_dir=`dirname $0`
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-rm ~/.vimrc ~/.tmux.conf
-ln -s ~/$work_dir/.vimrc ~/.vimrc
-vim +VundleInstall! +qa
-ln -s ~/$work_dir/.tmux.conf ~/.tmux.conf
