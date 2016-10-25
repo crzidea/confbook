3a6fefff26f5df2e0ef3a4523629ebc3b5ed3cb1
Crzidea <i@crzidea.com>
2016-10-25 05:49:03 +0000

    Auto install useful tools

--- install.sh
+++ install.sh
@@ -12,15 +12,40 @@ function insert_line() {
     echo "skip $line"
   fi
 }
-insert_line ~/.bashrc       "source $CONFBOOK_DIR/.bash_profile"
-insert_line ~/.vimrc        "source $CONFBOOK_DIR/.vimrc"
-insert_line ~/.tmux.conf    "source-file $CONFBOOK_DIR/.tmux.conf"
-insert_line ~/.tmux.conf    "source-file $CONFBOOK_DIR/.tmux-v1.9.conf"
 
-     ln -s $CONFBOOK_DIR/.editorconfig  ~/
-     ln -s $CONFBOOK_DIR/.openssl.cnf   ~/
-     ln -s $CONFBOOK_DIR/ssh_config     ~/.ssh/config
-sudo ln -s $CONFBOOK_DIR/gitconfig      /etc/gitconfig
+# Configurations in $HOME
+insert_line ~/.bashrc     "source $CONFBOOK_DIR/.bash_profile"
+insert_line ~/.vimrc      "source $CONFBOOK_DIR/.vimrc"
+insert_line ~/.tmux.conf  "source-file $CONFBOOK_DIR/.tmux.conf"
+insert_line ~/.tmux.conf  "source-file $CONFBOOK_DIR/.tmux-v1.9.conf"
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s $CONFBOOK_DIR/.editorconfig ~/
+ln -s $CONFBOOK_DIR/.openssl.cnf  ~/
+ln -s $CONFBOOK_DIR/ssh_config    ~/.ssh/config
+
+# Add sudoer
+#group=$(groups | awk '{print $1}')
+#sudo insert_line /etc/sudoers.d/$USER "${group} ALL=(ALL) NOPASSWD:ALL"
+
+sudo ln -s $CONFBOOK_DIR/gitconfig /etc/gitconfig
+
+function clone_or_fetch() {
+  url=$1
+  dir=$2
+  if [[ -d $dir ]]; then
+    cd $dir
+    git fetch origin
+  else
+    git clone $url $dir
+    cd $dir
+  fi
+  git checkout HEAD
+}
+
+# Install Vundle
+clone_or_fetch https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall! +qall
+
+# Useful tools
+mkdir -p ~/.local
+clone_or_fetch https://github.com/crzidea/bin.git ~/.local/bin
